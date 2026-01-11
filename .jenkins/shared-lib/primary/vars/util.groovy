79afed540f09d0a3d08f5394cdffa3b9a164f58c
JB Ladera <minidomou@gmail.com>
2026-01-11 02:34:57 -0600

    forgot to adjust workspace
    [backend,frontend,run,default]

--- .jenkins/shared-lib/primary/vars/util.groovy
+++ .jenkins/shared-lib/primary/vars/util.groovy
@@ -15,7 +15,7 @@ def printMap(Map map) {
 
 def loadScript(Map params = [:]) {
     def name = params.name
-    def path = "./${name}"
+    def path = "${env.WORKSPACE}/${name}"
 
     echo "load script path: ${path}"
 
