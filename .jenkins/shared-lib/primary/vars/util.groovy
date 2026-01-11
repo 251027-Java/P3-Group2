5d86e9c9c5ef8ed88b6282b752501df949090e18
JB Ladera <minidomou@gmail.com>
2026-01-11 12:56:28 -0600

    run ls
    [backend,frontend,run]

--- .jenkins/shared-lib/primary/vars/util.groovy
+++ .jenkins/shared-lib/primary/vars/util.groovy
@@ -15,7 +15,7 @@ def printMap(Map map) {
 
 def loadScript(Map params = [:]) {
     def name = params.name
-    def path = "${env.WORKSPACE}/${name}"
+    def path = "./${name}"
     def safeName = path.replaceAll(/[\/@]/, '-')
 
     echo "load script path: ${path}"
@@ -27,6 +27,8 @@ def loadScript(Map params = [:]) {
     }
 
     unstash name: safeName
+
+    sh 'ls -la'
 
     return path
 }
