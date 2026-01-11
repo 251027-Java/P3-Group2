3fea622043c6080341fc00cc6c362b422f4a58fe
JB Ladera <minidomou@gmail.com>
2026-01-11 15:09:35 -0600

    include command in summary for most
    [backend,run]

--- .jenkins/shared-lib/primary/vars/staging.groovy
+++ .jenkins/shared-lib/primary/vars/staging.groovy
@@ -6,28 +6,39 @@ def lint(Map params = [:]) {
     checksUtil.pending name: name
 
     def successRet = false
+    def summary = """
+```sh
+${settings.lint.command}
+```
+""".trim()
 
     dir(path) {
         try {
             sh "${settings.lint.command}"
-            checksUtil.success name: name
+            checksUtil.success name: name, summary: summary
             successRet = true
         } catch (err) {
             echo "${err}"
             pipelineUtil.failStage()
-            checksUtil.failed name: name
+            checksUtil.failed name: name, summary: summary
         }
     }
 
     return successRet
 }
 
+// assumes java. not expecting tests for frontend
 def test(Map params = [:]) {
     def path = params.path
     def settings = params.settings
 
     def name = "test / ${checksUtil.nameFromDirectory([path: path])}"
     def successRet = false
+    def summary = """
+```sh
+${settings.lint.command}
+```
+""".trim()
 
     withChecks(name: name) {
         dir(path) {
@@ -38,7 +49,7 @@ def test(Map params = [:]) {
             } catch (err) {
                 echo "${err}"
                 pipelineUtil.failStage()
-                checksUtil.failed name: name
+                checksUtil.failed name: name, summary: summary
             }
         }
     }
@@ -54,16 +65,21 @@ def build(Map params = [:]) {
     checksUtil.pending name: name
 
     def successRet = false
+    def summary = """
+```sh
+${settings.lint.command}
+```
+""".trim()
 
     dir(path) {
         try {
             sh "${settings.build.command}"
-            checksUtil.success name: name
+            checksUtil.success name: name, summary: summary
             successRet = true
         } catch (err) {
             echo "${err}"
             pipelineUtil.failStage()
-            checksUtil.failed name: name
+            checksUtil.failed name: name, summary: summary
         }
     }
 
@@ -78,54 +94,63 @@ def execute(Map params = [:]) {
 
     pipelineUtil.getQualifyingDirs().each { path ->
         if (!path.startsWith(baseDirectory)) { return }
+        allSuccessful &= executeDir dockerCredentialsId: dockerCredentialsId, attributes: attributes
+    }
 
-        def shouldRun = (
-            !attributes['skip']
-            && (
-                attributes['run']
-                || (
-                    (attributes['pr:default'] || attributes['default'])
-                    && (attributes["change:${path}".toString()])
-                )
+    return allSuccessful
+}
+
+def executeDir(Map params = [:]) {
+    def dockerCredentialsId = params.dockerCredentialsId
+    def attributes = params.attributes
+    def allSuccessful = true
+
+    def shouldRun = (
+        !attributes['skip']
+        && (
+            attributes['run']
+            || (
+                (attributes['pr:default'] || attributes['default'])
+                && (attributes["change:${path}".toString()])
             )
         )
+    )
 
-        def settings = pipelineUtil.getSettings path: path
+    def settings = pipelineUtil.getSettings path: path
 
-        if (settings.lint.enabled && shouldRun) {
-            stage("lint ${path}") {
-                def success = lint path: path, settings: settings
-                allSuccessful &= success
-            }
+    if (settings.lint.enabled && shouldRun) {
+        stage("lint ${path}") {
+            def success = lint path: path, settings: settings
+            allSuccessful &= success
         }
+    }
 
-        if (settings.test.enabled && shouldRun) {
-            stage("test ${path}") {
-                def success = test path: path, settings: settings
-                allSuccessful &= success
-            }
+    if (settings.test.enabled && shouldRun) {
+        stage("test ${path}") {
+            def success = test path: path, settings: settings
+            allSuccessful &= success
         }
+    }
 
-        if (settings.build.enabled && shouldRun) {
-            stage("build ${path}") {
-                def success = build path: path, settings: settings
-                allSuccessful &= success
-                attributes["build:${path}".toString()] = success
-            }
+    if (settings.build.enabled && shouldRun) {
+        stage("build ${path}") {
+            def success = build path: path, settings: settings
+            allSuccessful &= success
+            attributes["build:${path}".toString()] = success
         }
+    }
 
-        def shouldBuildImage = (
-            attributes["build:${path}".toString()]
-            || attributes['imageall']
-            || attributes["image:${path}".toString()]
-        )
+    def shouldBuildImage = (
+        attributes["build:${path}".toString()]
+        || attributes['imageall']
+        || attributes["image:${path}".toString()]
+    )
 
-        if (settings.image.enabled && shouldBuildImage) {
-            stage("image ${path}") {
-                def success = dockerUtil.image path: path, settings: settings,
-                    credId: dockerCredentialsId, latest: attributes['default']
-                allSuccessful &= success
-            }
+    if (settings.image.enabled && shouldBuildImage) {
+        stage("image ${path}") {
+            def success = dockerUtil.image path: path, settings: settings,
+                credId: dockerCredentialsId, latest: attributes['default']
+            allSuccessful &= success
         }
     }
 
