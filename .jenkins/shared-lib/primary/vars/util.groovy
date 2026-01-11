ffb9eb47119a4e9c27f2b3deb6d5f3cc58ae6e29
JB Ladera <minidomou@gmail.com>
2026-01-11 12:54:09 -0600

    replace @
    [backend,frontend,run]

--- .jenkins/shared-lib/primary/vars/util.groovy
+++ .jenkins/shared-lib/primary/vars/util.groovy
@@ -16,7 +16,7 @@ def printMap(Map map) {
 def loadScript(Map params = [:]) {
     def name = params.name
     def path = "${env.WORKSPACE}/${name}"
-    def safeName = path.replaceAll('/', '-')
+    def safeName = path.replaceAll(/[\/@]/, '-')
 
     echo "load script path: ${path}"
 
