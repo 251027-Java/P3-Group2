8a39bea14bee74eb3e1fd2ef702a8210a822c857
Marcus Wright <marcus03wright@icloud.com>
2026-01-16 18:42:59 -0600

    Updated card service to act as a eureka client, updated pom and yaml

--- backend/card-service/src/main/java/com/marketplace/card/CardApplication.java
+++ backend/card-service/src/main/java/com/marketplace/card/CardApplication.java
@@ -4,11 +4,13 @@ package com.marketplace.card;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Main entry point for the Card Service application.
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CardApplication {
 
 	public static void main(String[] args) {
