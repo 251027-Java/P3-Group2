dfc79572f1e54b2c5b2a18d035b25d84921ec8b8
manas <manasvini569@revature.net>
2026-01-20 10:31:49 -0600

    AngularSSPA application running

--- frontend/AngularSSPA/src/main.single-spa.ts
+++ frontend/AngularSSPA/src/main.single-spa.ts
@@ -5,19 +5,18 @@ import { Router, NavigationStart } from '@angular/router';
 
 import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
 
-
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 import { singleSpaPropsSubject } from './single-spa/single-spa-props';
 
-if (environment.production) {
-  enableProdMode();
-}
+// if (environment.production) {
+//   enableProdMode();
+// }
 
 const lifecycles = singleSpaAngular({
   bootstrapFunction: singleSpaProps => {
     singleSpaPropsSubject.next(singleSpaProps);
-    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
+    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppComponent);
   },
   template: '<app-root />',
   Router,
