5947d27fe76e8bc795e60db49f21e250217bd925
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2026-01-20 13:21:39 -0600

    Starting remote brnahc for angular setup

--- frontend/AngularSSPA/src/main.single-spa.ts
+++ frontend/AngularSSPA/src/main.single-spa.ts
@@ -1,27 +1,19 @@
+import './polyfills';
+
 import { enableProdMode, NgZone } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { singleSpaAngular } from 'single-spa-angular';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Router, NavigationStart } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-import { singleSpaAngular, getSingleSpaExtraProviders } from 'single-spa-angular';
-
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import { singleSpaPropsSubject } from './single-spa/single-spa-props';
-
-if (environment.production) {
-  enableProdMode();
-}
+// Optional: only use this if you actually have src/environments/environment.ts
+// import { environment } from './environments/environment';
+// if (environment.production) enableProdMode();
 
 const lifecycles = singleSpaAngular({
-  bootstrapFunction: singleSpaProps => {
-    singleSpaPropsSubject.next(singleSpaProps);
-    return platformBrowserDynamic(getSingleSpaExtraProviders()).bootstrapModule(AppModule);
-  },
+  bootstrapFunction: () => bootstrapApplication(AppComponent, appConfig),
   template: '<app-root />',
-  Router,
-  NavigationStart,
   NgZone,
 });
 
