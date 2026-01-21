504e2a394d8961aafc8e02cb274611a4479be07a
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2026-01-21 14:35:43 -0600

    marketplace and trade pages

--- frontend/AngularSSPA/src/app/app.routes.ts
+++ frontend/AngularSSPA/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { MarketplacePageComponent } from './pages/marketplace-page/marketplace-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { TradePageComponent } from './pages/trade-page/trade-page.component';
 import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 export const routes: Routes = [
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
   { path: 'marketplace', component: MarketplacePageComponent },
+  { path: 'trade', component: TradePageComponent },
   { path: '**', component: EmptyRouteComponent }
 ];
--- frontend/AngularSSPA/src/app/pages/marketplace-page/marketplace-page.component.ts
+++ frontend/AngularSSPA/src/app/pages/marketplace-page/marketplace-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-marketplace-page',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './marketplace-page.component.html',
   styleUrl: './marketplace-page.component.css'
 })
