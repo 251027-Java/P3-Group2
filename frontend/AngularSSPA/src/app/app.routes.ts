8959501c98b8d1ca82cc6a359b61f4279a5aa4d0
Natalia Orlof-Carson <norlofcarson@sandiego.edu>
2026-01-21 11:59:19 -0600

    marketplace is up but not full screen

--- frontend/AngularSSPA/src/app/app.routes.ts
+++ frontend/AngularSSPA/src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { MarketplacePageComponent } from './pages/marketplace-page/marketplace-page.component';
 import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
   { path: 'signup', component: SignupPageComponent },
   { path: 'marketplace', component: MarketplacePageComponent },
-  { path: '**', redirectTo: 'login' }
+  { path: '**', component: EmptyRouteComponent }
 ];
