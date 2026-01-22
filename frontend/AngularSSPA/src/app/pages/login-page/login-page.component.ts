0d87fbb002086129c3aa2d10ce1bea98985120f4
manas <manasvini569@revature.net>
2026-01-22 09:27:48 -0600

    Building login component and setting up auth service

--- frontend/AngularSSPA/src/app/pages/login-page/login-page.component.ts
+++ frontend/AngularSSPA/src/app/pages/login-page/login-page.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
-export class LoginPageComponent {}
+export class LoginPageComponent {
+  email : string = "";
+  password: string = "";
+
+  constructor(private router: Router) {}
+
+  onSubmit() {
+    // Call to auth service to login
+    console.log('Login with this email:', this.email, ' and this password:', this.password);
+
+    this.router.navigateByUrl('marketplace');
+  }
+}
