948786fbdfdd5960aa84bc06e619c81949390fa3
manas <manasvini569@revature.net>
2026-01-22 11:13:18 -0600

    Added token storage for jwt

--- frontend/AngularSSPA/src/app/services/auth.service.ts
+++ frontend/AngularSSPA/src/app/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 interface AuthResponse{
@@ -21,20 +21,43 @@ export class AuthService {
   login(email:string, password: string): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${environment.apiUrl}/api/auth/login`, {
       email, password
-    });
+    }).pipe(
+      tap(response => {
+        this.setJWT(response.token);
+        this.setAuthentication(true);
+      })
+    );
   }
 
   register(email: string, username: string, password: string): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${environment.apiUrl}/api/auth/register`, {
       email, username, password
-    });
+    }).pipe(
+      tap(response => {
+        this.setJWT(response.token);
+        this.setAuthentication(true);
+      })
+    );
   }
 
-  setAuthentication(): void {
-    this._isAuthenticated = true;
+  setAuthentication(status: boolean): void {
+    this._isAuthenticated = status;
   }
 
   getAuthentication(): boolean {
     return this._isAuthenticated;
+  }
+
+  private setJWT(token: string): void {
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  logout(): void {
+    localStorage.removeItem(this.TOKEN_KEY);
+    this._isAuthenticated = false;
   }
 }
