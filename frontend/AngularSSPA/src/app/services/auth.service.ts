6ef5ae7bbf3fea7ddfc9f85987703d6dc6ec1fc3
manas <manasvini569@revature.net>
2026-01-22 10:03:57 -0600

    added login method to auth service

--- frontend/AngularSSPA/src/app/services/auth.service.ts
+++ frontend/AngularSSPA/src/app/services/auth.service.ts
@@ -1,10 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface AuthResponse{
+  token: string,
+  expiresIn: number,
+  username: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private _isAuthenticated = false;
+  private _authHeader?: string;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  login(email:string, password: string): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>('${environment.apiUrl}/api/auth/login', {
+      email, password
+    });
+  }
+
+  authenticateUser(email: string, password: string): void {
+    this._isAuthenticated = true;
+    //
+  }
 }
